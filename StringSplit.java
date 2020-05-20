af55098f56f35bc8f522a62d6539ca6cc2254eb0
ralph666888 <63554630+ralph666888@users.noreply.github.com>
2020-05-20 14:36:48 -0400

    Update StringSplit.java

--- StringSplit.java
+++ StringSplit.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class StringSplit {
     public static String[] split(String s, String regex){
         String[]tokens = s.split(regex);
-        String[]aftersplit=new String[2*tokens.length-1];
-        for(int i=0;i<tokens.length;i++){
+        String[]aftersplit = new String[2*tokens.length-1];
+        for(int i = 0;i<tokens.length;i++){
             aftersplit[2*i] = tokens[i];
         }
-        for(int i=0;i<(aftersplit.length-1)/2;i++){
+        for(int i = 0;i<(aftersplit.length-1)/2;i++){
             aftersplit[2*i+1] = regex;
 
         }
@@ -15,7 +15,7 @@ public class StringSplit {
     }
 
     public static void main(String[]args){
-        String s="ab#12#453";
+        String s = "ab#12#453";
 
         for(String i:split(s,"#")){
             System.out.println(i);
