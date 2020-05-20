f8e7838d6c1cb2435e9f1cdea138b9d704e98f7f
ralph666888 <63554630+ralph666888@users.noreply.github.com>
2020-05-20 14:34:25 -0400

    Update MyRectangle2D.java

--- MyRectangle2D.java
+++ MyRectangle2D.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class MyRectangle2D {
    private double x;
    private double y;
-   private double width = 1; ///not how you do default values
-   private double height = 1;
+   private double width; ///not how you do default values
+   private double height;
    public MyRectangle2D(){}; //search up default contructor
    public MyRectangle2D(double x, double y,double width,double height){
        this.x = x;
@@ -16,25 +16,25 @@ public class MyRectangle2D {
        this(1, 1, width, height);
    }
    public void setX(double x2){
-       this.x=x2;
+       this.x = x2;
    }
    public double getX(){
        return x;
    }
    public void setY(double y2){
-        this.y=y2;
+        this.y = y2;
    }
    public double getY(){
         return y;
    }
     public void setWidth(double width2){
-        this.width=width2;
+        this.width = width2;
    }
     public double getWidth(){
         return width;
    }
     public void setHeight(double height2){
-        this.height=height2;
+        this.height = height2;
    }
     public double getHeight(){
         return height;
@@ -46,24 +46,24 @@ public class MyRectangle2D {
        return 2*(width+height);
    }
    public boolean contains(double x3, double y3){
-       if(Math.abs(x3-x)<=0.5*width && Math.abs(y3-y)<=0.5*height)
+       if(Math.abs(x3-x) <= 0.5*width && Math.abs(y3-y) <= 0.5*height)
            return true;
        else
            return false;
    }
    public boolean contains(MyRectangle2D r){
-       if(Math.abs(r.getX()-x)+0.5*r.getWidth()<=0.5*width && Math.abs(r.getY()-y)+0.5*r.getHeight()<=0.5*height)
+       if(Math.abs(r.getX()-x)+0.5*r.getWidth() <= 0.5*width && Math.abs(r.getY()-y)+0.5*r.getHeight() <= 0.5*height)
            return true;
        else
            return false;
    }
    public boolean overlaps(MyRectangle2D r){
-       if(Math.abs(r.x-x)>0.5*(r.width+width)||Math.abs(r.y-y)>0.5*(r.height+height)){
+       if(Math.abs(r.x-x)>0.5*(r.width+width)||Math.abs(r.y-y)>0.5*(r.height+height))
            return false;
-       } //keep brackets consistant
-       else{
+        //keep brackets consistant
+       else
            return true;
-       }
+       
 
    }
    public static void main(String[]args){
